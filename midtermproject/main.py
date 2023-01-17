c2047c1f5a394530ddc91aa186759c28a2f9f524
frank-cywong <90653148+frank-cywong@users.noreply.github.com>
2023-01-17 11:38:29 -0500

    addedge and deledge commands

--- midtermproject/main.py
+++ midtermproject/main.py
@@ -44,6 +44,8 @@ def print_help():
     print("importgraph: Allows you to input a graph via a adjacency matrix.")
     #print("printgraph: Prints the current graph in the form of an adjacency matrix.") - not needed, use showgraph instead
     print("showgraph: Displays the current graph with Matplotlib.")
+    print("addedge [node_a] [node_b]: (example: addedge 1 2) Adds a (directed) edge from node_a to node_b (Note that nodes are 0-indexed).")
+    print("deledge [node_a] [node_b]: (example: deledge 1 2) Deletes a (directed) edge (if it exists) from node_a to node_b (Note that nodes are 0-indexed).")
     print("resetgraph: Resets the current graph to the default graph.")
     print("")
 
@@ -56,19 +58,52 @@ def main():
     while True: # infinite loop
         print("Type in command to execute, or \"help\" for a list of available commands!")
         cmd = input()
+        # splits input into an array of tokens by spaces
+        args = cmd.split(" ")
+        # command is the first argument
+        cmd = args[0]
+        # the other arguments are actual arguments
+        args = args[1:]
         # on one hand why does python have no switch statements
         # on the other hand in C i'd be using strncmp so this is also not that bad actually
         if(cmd == "help"):
             print_help();
-        if(cmd == "resetgraph"):
+        elif(cmd == "resetgraph"):
             current_graph = load_default_graph();
-        if(cmd == "showgraph"):
+        elif(cmd == "showgraph"):
             plt.clf();
-            nx.draw(current_graph);
+            nx.draw(current_graph, with_labels = True);
             plt.show();
-            printf("")
-        if(cmd == "exit"):
+            print("")
+        elif(cmd == "addedge"):
+            if(len(args) < 2):
+                print("addedge requires 2 arguments!")
+            else:
+                try:
+                    current_graph.add_edge(int(args[0]), int(args[1]))
+                    print("Successfully added edge!")
+                # if int() fails, arguments isn't an integer, show user an error
+                except ValueError:
+                    print("Edge indices must be an integer!")
+            print("")
+        elif(cmd == "deledge"):
+            if(len(args) < 2):
+                print("deledge requires 2 arguments!")
+            else:
+                try:
+                    current_graph.remove_edge(int(args[0]), int(args[1]))
+                    print("Successfully deleted edge!")
+                # if int() fails, arguments isn't an integer, show user an error
+                except ValueError:
+                    print("Edge indices must be an integer!")
+                except nx.NetworkXError:
+                    print("No edge found between nodes {} and {} to remove!".format(int(args[0]), int(args[1])));
+            print("")
+        elif(cmd == "exit"):
             print("Exiting program!");
+            break;
+        else:
+            print("Command not recognized!");
             break;
 
 # Boilerplate to make Python behave more like C or Java by making it have a main function
