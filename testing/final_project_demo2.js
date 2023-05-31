66f58bf64c1e4a7debbe5f447c990821dc3da5be
frank-cywong <90653148+frank-cywong@users.noreply.github.com>
2023-05-30 20:21:28 -0400

    Finish demo 2

--- testing/final_project_demo2.js
+++ testing/final_project_demo2.js
@@ -1,4 +1,4 @@
-const params = {"v_input": [1, 0], "m1": [[0.5, 0.5], [0.5, 0.5]], "b1": [0, 0], "m2": [[0.8, 0.4]], "b2": 0.1};
+const params = {"v_input": [1, 0], "m1": [[0.5, 0.5], [0.5, 0.5]], "b1": [0, 0], "m2": [[0.8, 0.4]], "b2": -0.3};
 
 const intermediates = {"v2": [-1, -1], "v3": [-1, -1], "v4": -1, "v5": -1};
 
@@ -25,12 +25,12 @@ function compute_intermediate_values(){
 }
 
 function customround(x){
-	return (Math.round(x * 100) / 100).toString();
+	return (Math.round(x * 100) / 100);
 }
 
 function load_text_on_canvas(){
 	const canvas = document.getElementById("neuralNetworkGraph");
-	const multi = (window.innerWidth >= 900 ? 3 : 1.2);
+	const multi = (window.innerWidth >= 900 ? 3 : 1.3);
 	if(canvas.getContext){
 		const ctx = canvas.getContext("2d");
 		ctx.font = "12px Arial";
@@ -69,18 +69,59 @@ function load_text_on_canvas(){
 		canvas_arrow(ctx, multi * 25 + 45, 100, multi * 150 - 45, 100);
 		canvas_arrow(ctx, multi * 150 - 15, 100, multi * 150 + 15, 100);
 		canvas_arrow(ctx, multi * 150 - 15, 200, multi * 150 + 15, 200);
+		canvas_arrow(ctx, multi * 150 + 45, 25, multi * 275 - 75, 150);
+		canvas_arrow(ctx, multi * 150 + 45, 100, multi * 275 - 75, 150);
+		canvas_arrow(ctx, multi * 150 + 45, 200, multi * 275 - 75, 150);
 		canvas_arrow(ctx, multi * 275 - 45, 150, multi * 275 - 15, 150);
 		ctx.strokeStyle = "rgba(0, 0, 0, 1)";
 		ctx.stroke();
+		// weights text
+		ctx.font = "12px Arial";
+		ctx.fillStyle = "rgba(0, 0, 0, 1)";
+		ctx.textAlign = "left";
+		ctx.fillText("×" + customround(params["b1"][0]), multi * 25 + 81, 28);
+		ctx.fillText("×" + customround(params["b1"][1]), multi * 25 + 81, 70);
+		ctx.fillText("×" + customround(params["m1"][0][0]), multi * 25 + 81, 90);
+		ctx.fillText("×" + customround(params["m1"][1][0]), multi * 25 + 81, 132);
+		ctx.fillText("×" + customround(params["m1"][0][1]), multi * 25 + 81, 168);
+		ctx.fillText("×" + customround(params["m1"][1][1]), multi * 25 + 81, 212);
+		ctx.fillText("×" + customround(params["b2"]), multi * 150 + 51, 24);
+		ctx.fillText("×" + customround(params["m2"][0][0]), multi * 150 + 51, 95);
+		ctx.fillText("×" + customround(params["m2"][0][1]), multi * 150 + 51, 207);
 	}
+	const responsive = (window.innerWidth < 600);
+	const tString1 = `\$\$\\text{Inputs: }\\vec{v_1} = \\begin{bmatrix}${params["v_input"][0]}\\\\${params["v_input"][1]}\\end{bmatrix}, W_1 = \\begin{bmatrix}${params["m1"][0][0]} && ${params["m1"][0][1]} \\\\ ${params["m1"][1][0]} && ${params["m1"][1][1]} \\end{bmatrix}, \\vec{b_1} = \\begin{bmatrix}${params["b1"][0]}\\\\${params["b1"][1]}\\end{bmatrix}, W_2 = \\begin{bmatrix}${params["m2"][0][0]} && ${params["m2"][0][1]} \\end{bmatrix}, \\vec{b_2} = \\begin{bmatrix}${params["b2"]}\\end{bmatrix}\$\$`;
+	const tStringM1 = `\$\$\\text{Inputs: }\\vec{v_1} = \\begin{bmatrix}${params["v_input"][0]}\\\\${params["v_input"][1]}\\end{bmatrix}, W_1 = \\begin{bmatrix}${params["m1"][0][0]} && ${params["m1"][0][1]} \\\\ ${params["m1"][1][0]} && ${params["m1"][1][1]} \\end{bmatrix}\$\$`
+	const tStringM2 = `\$\$\\vec{b_1} = \\begin{bmatrix}${params["b1"][0]}\\\\${params["b1"][1]}\\end{bmatrix}, W_2 = \\begin{bmatrix}${params["m2"][0][0]} && ${params["m2"][0][1]} \\end{bmatrix}, \\vec{b_2} = \\begin{bmatrix}${params["b2"]}\\end{bmatrix}\$\$`;
+	const im0 = `\\text{Computed Values: }\\vec{v_2} = W_1\\vec{v_1}+\\vec{b_1} = \\begin{bmatrix}${customround(intermediates["v2"][0])}\\\\${customround(intermediates["v2"][1])}\\end{bmatrix}`;
+	const im1 = `\\vec{v_3} = f(\\vec{v_2}) = \\begin{bmatrix}${customround(intermediates["v3"][0])}\\\\${customround(intermediates["v3"][0])}\\end{bmatrix}`;
+	const im2 = `\\vec{v_4} = W_2\\vec{v_3}+\\vec{b_2} = \\begin{bmatrix}${customround(intermediates["v4"])}\\end{bmatrix}`;
+	const im3 = `\\vec{v_5} = g(\\vec{v_4}) = \\begin{bmatrix}${customround(intermediates["v5"])}\\end{bmatrix}`;
+	
+	if(responsive){
+		document.getElementById("expression").innerText = tStringM1;
+		document.getElementById("expressionm").innerText = tStringM2;
+		document.getElementById("im0").innerText = "\$\$" + im0 + "\$\$";
+		document.getElementById("im1").innerText = "\$\$" + im1 + "\$\$";
+		document.getElementById("im2").innerText = "\$\$" + im2 + "\$\$";
+		document.getElementById("im3").innerText = "\$\$" + im3 + "\$\$";
+	} else {
+		document.getElementById("expression").innerText = tString1;
+		document.getElementById("expressionm").innerText = "";
+		document.getElementById("im0").innerText = "\$\$" + im0 + ", " + im1 + "\$\$";
+		document.getElementById("im1").innerText = "\$\$" + im2 + ", " + im3 + "\$\$";
+		document.getElementById("im2").innerText = "";
+		document.getElementById("im3").innerText = "";
+	}
+	MathJax.typeset();
 }
 
 function draw_network(){
   compute_intermediate_values();
   const canvas = document.getElementById("neuralNetworkGraph");
   const width = window.innerWidth;
-  canvas.width = (width >= 900 ? 900 : 360);
-  const multi = (width >= 900 ? 3 : 1.2);
+  canvas.width = (width >= 900 ? 900 : 390);
+  const multi = (width >= 900 ? 3 : 1.3);
   if (canvas.getContext) {
 	const ctx9 = canvas.getContext("2d");
 	ctx9.beginPath();
@@ -166,10 +207,53 @@ function draw_network(){
 window.onload = (event) => {
   console.log("page is fully loaded");
   draw_network();
+  console.log(window.innerWidth);
+  if(window.innerWidth < 600){
+	  console.log("aidsninda");
+	  document.getElementById("pcver1").innerText = "$$\\text{Activation Function: } f(x) = \\begin{cases} x \\text{ if } x \\ge 0 \\\\ 0 \\text { if } x \\lt 0 \\end{cases}$$";
+	  document.getElementById("mobileonly1").innerText = "$$\\text{Output Function: } g(x) = \\begin{cases} 1 \\text{ if } x \\ge 0.5 \\\\ 0 \\text{ if } x \\lt 0.5 \\end{cases}$$";
+	  MathJax.typeset();
+  }
 };
-document.write("Hello world!");
+//document.write("Hello world!");
 
 function update(){
 	console.log("H");
+	const mappings = [
+		// param name, index 0, index 1, id
+		["v_input", 0, -1, "vinput1"],
+		["v_input", 1, -1, "vinput2"],
+		["m1", 0, 0, "w111"],
+		["m1", 0, 1, "w112"],
+		["m1", 1, 0, "w121"],
+		["m1", 1, 1, "w122"],
+		["b1", 0, -1, "b11"],
+		["b1", 1, -1, "b12"],
+		["m2", 0, 0, "w211"],
+		["m2", 0, 1, "w212"],
+		["b2", -1, -1, "b21"]
+	];
+	mappings.forEach((mapping) => {
+		const target_element = document.getElementById(mapping[3]);
+		targetValue = parseFloat(target_element.value);
+		if(!isNaN(targetValue)){
+			console.log(targetValue);
+			if(mapping[1] == -1){
+				params[mapping[0]] = customround(targetValue);
+			} else if (mapping[2] == -1){
+				params[mapping[0]][mapping[1]] = customround(targetValue);
+			} else {
+				params[mapping[0]][mapping[1]][mapping[2]] = customround(targetValue);
+			}
+		}
+		let param_element = params[mapping[0]];
+		if(mapping[1] != -1){
+			param_element = param_element[mapping[1]];
+		}
+		if(mapping[2] != -1){
+			param_element = param_element[mapping[2]];
+		}
+		target_element.value = param_element;
+	});
 	draw_network();
 }
